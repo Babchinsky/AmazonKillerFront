34f7fbdf0107cd15ea6c039a28b764585534b994
alexs <babchinsky.prog@gmail.com>
2025-06-18 21:19:46 +0300

    fix: add adminOnly flag to /admin route and remove unused ProtectedRoute version

--- perry/src/App.tsx
+++ perry/src/App.tsx
@@ -60,7 +60,7 @@ function App() {
 
           <Route path="/admin/login" element={<LoginPage />} />
           <Route path="/admin" element={
-            <ProtectedRoute>
+            <ProtectedRoute adminOnly>
               <AdminLayout />
             </ProtectedRoute>
           }>
